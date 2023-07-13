e60e52ba9d5cdc60d49ec6c4a296bc08540d1f1a
Probo Krishnacahya <probo.krishnacahya@gmail.com>
2023-07-13 16:28:19 +0700

    insert storyboard assets

---
+++ loume/Views/StoryBoardView/AfterGoalView.swift
@@ -0,0 +1,20 @@
+//
+//  AfterGoal.swift
+//  loume
+//
+//  Created by Probo Krishnacahya on 13/07/23.
+//
+
+import SwiftUI
+
+struct AfterGoalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AfterGoalView_Previews: PreviewProvider {
+    static var previews: some View {
+        AfterGoalView()
+    }
+}
